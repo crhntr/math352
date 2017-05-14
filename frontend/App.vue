dd3a60f3568bdf196f51c27a0e963282ab12378d
crhntr <crhntr.com@gmail.com>
2017-05-13 20:58:19 -0700

    ate a tomato mozzerella sandwitch from starbucks
    also modified the view and style

--- frontend/App.vue
+++ frontend/App.vue
@@ -1,32 +1,53 @@
 <template>
 	<div>
-		<div class="page">
-			<h2>STEP 1: Load Data From PubMed</h2>
-			<p>This step request articles from PubMed using a pubmed query ({{query}}). The pubmed api simply searches a database of articles by date added. The goal of this project is to filter these using a machine learning algorithm based on Naive Bayes classification.
-			</p>
-			<button @click="getItems()" v-if="!updating">Update Items</button>
-			<div>
-				<div v-for="item in items" class="article">
-					<h3 @click="item.show_body = !item.show_body">{{item.data.title}}</h3>
-					<p v-if="item.show_body">{{item.data.body}}</p>
-					<p v-if="item.show_body">{{item.data.categories}}</p>
+		<div class="page-container">
+			<div class="page">
+				<h1>Math352: Class Project</h1>
+				<a href="http://github.com/crhntr/math352" target="_blank">src</a>
+				<h2>Introduction</h2>
+				<p>During lecture we discussed Bayes' theorem, which describes the probability of an event based on some previous knowledge called the prior probability. Baysian Naive Classification is an algorithm in machine learning that uses Bayes' theorem to classify text, or sub-objects made up of sub objects. When categorizing text the sub-objects are words. The probability that a block of text is in a category is based on what words it contains and how many of each kind.</p>
+				<p>
+					<em>For a more detailed explanation of Baysian Naive Classification see: <a href="https://en.wikipedia.org/wiki/Naive_Bayes_classifier">https://en.wikipedia.org/wiki/Naive_Bayes_classifier.</a></em>
+				</p>
+				<p>The following classifier pulls Abstracts and Titles from the PubMed api. The abstract and title (article) are displayed to the user and the user assigns the article to a category. The algorithm then learns based on the categories given. When the articles are refreshed the user should see the probabilities that an article is in a category next to the name of a category for each article.</p>
+				<p>Although the program is relatively fast and efficient it is not too good at actually categorizing articles. However, I only manually categorized a few dozen articles in my tests. I will continue to work on a better application that allows for longer term data collection (this site drops all the data every so often) and takes into account where the words are (title, abstract…).</p>
+				<p>In machine learning, naive Bayes classifiers are a family of simple probabilistic classifiers based on applying Bayes' theorem with strong (naive) independence assumptions between the features.</p>
+				<div height="100vh">
+					<img src="/src/theorem.png" height="100px"/>
 				</div>
 			</div>
 		</div>
-		<div class="page classifier">
-			<h2>STEP 2: Classify Articles</h2>
-			<article>
-				<h2>{{item.data.title}}</h2>
-				<p>{{item.data.body}}</p>
-			</article>
-			<div class="widget">
-				<div v-for="(score, cat) in item.data.categories">
-					<input type="checkbox" :value="cat" :id="cat" v-model="item.categories">
-					<label :for="cat">{{cat}}</label>
+		<div class="page-container">
+			<div class="page">
+				<h2>STEP 1: Load Data From PubMed</h2>
+				<p>This step request articles from PubMed using a pubmed query ({{query}}). The pubmed api simply searches a database of articles by date added. The goal of this project is to filter these using a machine learning algorithm based on Naive Bayes classification. Every day the server requests articles matching the query in the last year. As users categorize articles the Baysian classifier can 'better' sort articles.
+				</p>
+				<button @click="getItems()" v-if="!updating">Update Items</button>
+				<div>
+					<div v-for="item in items" class="article">
+						<h3 @click="item.show_body = !item.show_body">{{item.data.title}}</h3>
+						<p v-if="item.show_body">{{item.data.body}}</p>
+						<p v-if="item.show_body">{{item.data.categories}}</p>
+					</div>
 				</div>
-				<input type="text" @keyup.enter="categories.push(newCategory)" v-model="newCategory" placeholder="Category"/>
-				<br />
-				<button @click="submitClasses(); next(); set_default_categories()">Next</button>
+			</div>
+		</div>
+		<div class="page-container">
+			<div class="page classifier">
+				<h2>STEP 2: Classify Articles</h2>
+				<article>
+					<h2>{{item.data.title}}</h2>
+					<p>{{item.data.body}}</p>
+				</article>
+				<div class="widget">
+					<div v-for="(score, cat) in item.data.categories">
+						<input type="checkbox" :value="cat" :id="cat" v-model="item.categories">
+						<label :for="cat">{{cat}}</label>
+					</div>
+					<input type="text" @keyup.enter="addNewCategory()" v-model="newCategory" placeholder="Category"/>
+					<br />
+					<button @click="submitClasses(); next(); set_default_categories()">Next</button>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -50,7 +71,9 @@
 				query: "talimogene laherparepvec [All Fields]"
 	    }
 	  },
-	  created() {},
+	  created() {
+			this.getItems()
+		},
 	  methods: {
 			submitClasses() {
 				if (this.item.categories.length > 0) {
@@ -83,6 +106,18 @@
 					console.log(response)
 					this.updating = false
 				})
+			},
+			addNewCategory() {
+				for (let cat in this.item.data.categories) {
+					if (this.newCategory === cat) {
+						this.newCategory = ""
+						console.log("not new")
+						return;
+					}
+				}
+				this.item.data.categories[this.newCategory] = {}
+				this.item.categories.push(this.newCategory)
+				this.newCategory = ""
 			},
  	    next() {
 				this.current_item_id++
